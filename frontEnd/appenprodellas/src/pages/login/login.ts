d07f4c5cbcd60e21f9644ad320dd023f4a86437c
cams1324 <camilo.grd04@gmail.com>
2018-11-07 21:02:05 -0500

    Se implementa validación de campos en el login con FormBuilder

--- frontEnd/appenprodellas/src/pages/login/login.ts
+++ frontEnd/appenprodellas/src/pages/login/login.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angu
 import { RestProvider } from '../../providers/rest/rest';
 import { BotonPanicoPage } from '../boton-panico/boton-panico';
 import { InicioappPage } from '../inicioapp/inicioapp';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 /**
  * Generated class for the LoginPage page.
@@ -18,47 +19,57 @@ import { InicioappPage } from '../inicioapp/inicioapp';
 })
 export class LoginPage {
 
-  email:any;
-  password:any;
+  email: any;
+  password: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public restProvider: RestProvider) {
+  formLogin: FormGroup;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder, public alertCtrl: AlertController, public restProvider: RestProvider) {
+    //Ejemplo uso de FormBuilder: https://blog.ng-classroom.com/blog/ionic2/validations-in-forms/
+    this.formLogin = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
 
-    if(window.localStorage['token']){
+    if (window.localStorage['token']) {
       this.navCtrl.setRoot(InicioappPage);
     }
   }
 
-  iniciarSesion(){
+  iniciarSesion() {
 
-    var data = {
-      'username': this.email,
-      'password': this.password
-    };
+    if (this.formLogin.valid) {
 
-    this.restProvider.login(data)
-    .then((result:any) => {
-      this.navCtrl.parent.getByIndex(0).show = false;
-      this.navCtrl.parent.getByIndex(4).show = true;
-      window.localStorage['token'] = result.key;
-      this.navCtrl.setRoot(BotonPanicoPage);
-    }, (err) => {
-      console.log("Ocurrio un error al intentar iniciar sesion");
-      console.log(err);
-      if (err.status === 400 ){
-        console.log("Ocurrio un error 400");
-        //console.log(err.error.non_field_errors[0]);
-        if( err.error.non_field_errors ){
-          console.log("es un error de non_field_errors");
-          this.mostrarAlertaOK('Error al iniciar sesión', err.error.non_field_errors[0]);
-        } else if ( err.error.password ) {
-          this.mostrarAlertaOK('Error al iniciar sesión', '(Contraseña). ' + err.error.password[0]);
-        }
-      }
-    });
+      var data = {
+        'username': this.email,
+        'password': this.password
+      };
+
+      this.restProvider.login(data)
+        .then((result: any) => {
+          this.navCtrl.parent.getByIndex(0).show = false;
+          this.navCtrl.parent.getByIndex(4).show = true;
+          window.localStorage['token'] = result.key;
+          this.navCtrl.setRoot(BotonPanicoPage);
+        }, (err) => {
+          console.log("Ocurrio un error al intentar iniciar sesion");
+          console.log(err);
+          if (err.status === 400) {
+            console.log("Ocurrio un error 400");
+
+            if (err.error.non_field_errors) {
+              this.mostrarAlertaOK('Error al iniciar sesión', err.error.non_field_errors[0]);
+            } else if (err.error.password) {
+              this.mostrarAlertaOK('Error al iniciar sesión', '(Contraseña). ' + err.error.password[0]);
+            }
+          }
+        });
+
+    }
   }
 
   mostrarAlertaOK(titulo, mensaje) {
